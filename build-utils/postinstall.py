8679e08421678728d6848b44a00739b62d7acd4a
Mehdi ESSALEHI <messalehi2001@outlook.com>
2022-03-23 01:57:46 +0100

    Small quick fix for .gitignore

---
+++ build-utils/postinstall.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from os import environ, path, chmod
+from subprocess import call
+
+prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+datadir = path.join(prefix, 'share')
+destdir = environ.get('DESTDIR', '')
+
+chmod(path.join(prefix, 'bin/atay'), 0o755)
+chmod(path.join(prefix, 'bin/atay-connector'), 0o755)
+
+if not destdir:
+    print('Updating icon cache...')
+    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
+
+    print('Updating desktop database...')
+    call(['update-desktop-database', '-q', path.join(datadir, 'applications')])
+
+    print('Compiling GSettings schemas...')
+    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
