aedc21f1433879a345a42da820c6fe93538589a3
Mehdi ESSALEHI <messalehi2001@outlook.com>
2022-05-10 12:44:31 +0100

    feat: Download detection improvements

--- extension/src/catcher.js
+++ extension/src/catcher.js
@@ -2,19 +2,75 @@ if (typeof browser === "undefined") {
     var browser = chrome;
 }
 
-browser.runtime.onMessage.addListener(function (request, _, __) {
-    browser.runtime.sendNativeMessage(
-        "com.github.essmehdi.flow", {
-            "url": request.url,
-            "headers": []
-        }
-    );
+const extensionsBlacklist = [
+    "png", "jpg", "jpeg", "gif"
+]
+
+// Context menu to force a download
+browser.menus.create({
+    id: "download-flow",
+    title: "Download with Flow",
+    contexts: ["link"]
 });
+
+// Context menu item clicked
+browser.menus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "download-flow") {
+        browser.runtime.sendNativeMessage(
+            "com.github.essmehdi.flow", {
+                "url": info.linkUrl,
+                "headers": []
+            }
+        );
+    }
+});
+
+function getFileDetailsDisposition(header) {
+    const match = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(header);
+    if (match && match[1]) {
+        const filename = match[1].trim();
+        const dotIndex = filename.lastIndexOf('.');
+        console.log(filename, dotIndex);
+        return {
+            filename,
+            extension: dotIndex > -1 ? filename.substring(dotIndex + 1) : ""
+        }
+    }
+}
+
+function getFileDetailsURL(url) {
+    const parsed = new URL(url);
+    const split = parsed.pathname.split('/');
+    const lastPath = split[split.length - 1];
+    const filename = lastPath || parsed.hostname;
+    const dotIndex = lastPath.lastIndexOf('.');
+    return {
+        filename,
+        extension: dotIndex > -1 ? filename.substring(dotIndex + 1) : ""
+    };
+    
+}
+
+function shouldCatch(details) {
+    const headers = details.responseHeaders;
+    const dispositionHeader = headers.find(header => header.name.toLowerCase() === 'content-disposition' && !header.value.startsWith("inline"));
+    
+    // Get file details from response
+    const filedetails = dispositionHeader ? getFileDetailsDisposition(dispositionHeader.value) : getFileDetailsURL(details.url);
+
+    // Download if there is 'Content-Disposition' or (file has an extension and not an browser openable file)
+    return dispositionHeader || 
+            (filedetails.extension 
+            && !extensionsBlacklist.includes(filedetails.extension)
+            && !headers.find(header => 
+                    header.name.toLowerCase() === 'content-type' && header.value.includes('text/html')
+                )
+            )
+}
 
 browser.webRequest.onHeadersReceived.addListener(function (details) {
     if (details.type !== 'main_frame' && details.type !== 'sub_frame') return;
-    const headers = details.responseHeaders;
-    if (headers.find(header => header.name.toLowerCase() === 'content-disposition' && !header.value.startsWith("inline"))) {
+    if (shouldCatch(details)) {
         browser.storage.local.get(details.requestId, function(item) {
             browser.runtime.sendNativeMessage(
                 "com.github.essmehdi.flow", {
